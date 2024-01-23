a656ae4852d1f2279db0611243849410eb5c009f
Malik Najeeb Ul Habib <maliknajeeb113@gmail.com>
2024-01-24 00:05:28 +0530

    Add tailwindcss to React project

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "../FoodApp/burger.png";
 
+/*
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - RestaurantContainer
+ *   - RestaurentCard
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
 
-const App = () =>{
-    
-    return (
-    <div>
-        
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="">
+        <img src={logo} className="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-    );
+  );
+};
 
-}
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
