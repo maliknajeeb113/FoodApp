ac0d879a6eb5e2ade41a95d9f3fc05840eb2a8ab
Malik Najeeb Ul Habib <maliknajeeb113@gmail.com>
2024-01-25 16:19:50 +0530

    use UseState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mock";
+import { useState } from "react";
 
 const Body = () => {
+
+  const [restaurantList,setRestaurantList] = useState(resList)
+
   return (
     <div className="body m-5 ">
       <div className="filter p-5">
         <button
           className="rounded bg-amber-600 p-2"
           onClick={() => {
-            console.log("click");
+
+            const filteredList = restaurantList.filter(res=>res.info.avgRating>4)
+            setRestaurantList(filteredList)
+            
           }}
         >
           Top Restaurants
         </button>
       </div>
       <div className="restaurant-container flex flex-wrap justify-center">
-        {resList.map((restaurant) => (
+        {restaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import burger from "../../src/assets/burger.png";
 
 const Header = () => {
     return (
-      <div className="header flex justify-between m-5 p-5 border-2 items-center">
+      <div className="header flex justify-between m-10 p-5 border-2 items-center">
         <div className="logo-container w-10">
           <img src={burger} className="logo" />
         </div>
