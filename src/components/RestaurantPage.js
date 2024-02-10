4d2dc8b35dd302453d21328980382eaf6f48a2c5
Malik Najeeb Ul Habib <maliknajeeb113@gmail.com>
2024-02-10 23:50:49 +0530

    fetch error fixed

--- src/components/RestaurantPage.js
+++ src/components/RestaurantPage.js
@@ -1,33 +1,37 @@
-import { useEffect, useState } from "react"
-import Shimmer from "./Shimmer"
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const RestaurantPage = () => {
+  const [resInfo, setResInfo] = useState(null);
 
-    const [resInfo,setResInfo] = useState(null)
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=312660&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = await data.json();
+    setResInfo(json);
+  };
 
-    const fetchMenu = async () =>{
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=16073&catalog_qa=undefined&submitAction=ENTER")
-        const json = await data.json()
-        setResInfo(json);
-    }
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
-    useEffect(()=>{
-        fetchMenu()
-    },[])
+  if (resInfo === null) return <Shimmer />;
+  const { name, cuisines } = resInfo?.data?.cards[0]?.card?.card?.info;
+  const { itemCards } =
+    resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2].card
+      .card;
 
-    // const name = resInfo?.data?.cards[0]?.card?.card?.info.name;
-    const {name, cuisines} = resInfo?.data?.cards[0]?.card?.card?.info || {}
+  return (
+    <div className="container flex flex-col items-center justify-center">
+      <div className="text-4xl font-bold">{name}</div>
+      <div>{cuisines.join(", ")}</div>
+      <ul>
+        {itemCards.map((item) => (
+          <li>{item.card.info.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    return resInfo===null?<Shimmer/>:(
-        <div>
-            <h1>{name}</h1>
-            <h2>{cuisines.join(", ")}</h2>
-            <ul>
-                <li>Biryani</li>
-                <li>Burger</li>
-                <li>Coffee</li>
-            </ul>
-        </div>
-    )
-}
-
-export default RestaurantPage
+export default RestaurantPage;
