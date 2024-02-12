3b05255f8f2c8af7e8c635892058d62ba57a4da3
Malik Najeeb Ul Habib <maliknajeeb113@gmail.com>
2024-02-12 17:58:43 +0530

    using div instead of h tags

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,10 +8,10 @@ const RestaurantCard = (props) => {
       <div className="hover:scale-105 transition duration-300 w-56 h-80 hover:bg-amber-600 hover:border-2 hover:border-amber-600 hover:cursor-pointer justify-center p-1 border-[1px]">
         <img src={CDN_URL + cloudinaryImageId} className="w-full max-h-32 object-cover" />
         <div className="">
-        <h2 className="pt-3 text-xl">{name}</h2>
-        <h4 className="pt-3 text-sm">{cuisines.join(", ")}</h4>
-        <h4 className="pt-3 text-sm">{avgRating} Stars</h4>
-        <h4 className="pt-3 text-sm">{sla?.slaString}</h4>
+        <div className="pt-3 text-xl">{name}</div>
+        <div className="pt-3 text-sm ">{cuisines.join(", ")}</div>
+        <div className="pt-3 text-sm">{avgRating} Stars</div>
+        <div className="pt-3 text-sm">{sla?.slaString}</div>
         </div>
       </div>
     );
